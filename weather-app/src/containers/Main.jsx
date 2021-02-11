91f30ad8e6dede0dbff69cb9726f9d7f568ef740
Ali <65448118+Ali-mustafa8@users.noreply.github.com>
2021-02-11 15:05:12 +0300

    Fetching the Api

---
+++ weather-app/src/containers/Main.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //axios use when you want to fetch data from external resource
+      const result = await axios(
+        `https://www.metaweather.com/api/location/search/?query=london`
+        //here you should enter the city name
+      );
+      console.log(result);
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((cityinfo) => (
+        <ul>
+          <li>{cityinfo.title}</li>
+          <li>{cityinfo.location_type}</li>
+          <li>{cityinfo.woeid}</li>
+          <li>{cityinfo.latt_long}</li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+/**
+ * LOOK THAT CODE WHEN YOU WANT TO CREATE A INPUR FOR SEARCH
+ * const Handle = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    if (name === "city") {
+      Setdata({
+        city: value,
+      });
+    }
+   
+    }
+  
+ THIS IS FOR A SEARCH BUTTON
+  <input
+          type="text"
+          name="city"
+          placeholde=""
+          onChange={(e) => Handle(e)}
+          //this e is like input
+        ></input>
+
+ */
+
+export default App;
