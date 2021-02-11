2bf37d5a25a9951db5876b7d192f32216b74a859
unknown <ismail.th.khalil@gmail.com>
2021-02-11 16:17:04 +0300

    add components to show the main data

---
+++ weather-app/src/components/MainData/MainData.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MainData() {
+    return (
+        <div>
+            <span>24&deg;</span>
+            <div>
+                <h5>Paris</h5>
+                <p>18:00 - Tue 25 Jan 20</p>
+            </div>
+            {/* add an svg here to show the weather condition */}
+        </div>
+    )
+}
